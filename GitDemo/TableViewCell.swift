64049e993094f3164f1f0916f023a97be282c91f
Akash Verma <akash.verma@srmtechsol.com>
2017-11-17 12:41:23 +0530

    Demo Branch Commit

---
+++ GitDemo/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  GitDemo
+//
+//  Created by Akash Verma on 17/11/17.
+//  Copyright © 2017 Akash Verma. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
